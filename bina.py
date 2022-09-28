5af0732e6856fa1f525893511c8ced89df4d20c1
qcuongning <nihnquoccuong@gmail.com>
2022-09-28 22:27:47 +0700

    daily rsi ma 99

--- bina.py
+++ bina.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 from binance import Client
 from backtesting.lib import resample_apply
-
+import ipdb
 urllib3.disable_warnings()
 
 
@@ -19,59 +19,45 @@ def RSI(array, n):
     loss = gain.copy()
     gain[gain < 0] = 0
     loss[loss > 0] = 0
-    rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
+    rs = gain.rolling(n).mean() / loss.abs().rolling(n).mean()
     return 100 - 100 / (1 + rs)
+
+def stoch_rsi(array, n = 14):
+    rsi = RSI(array, n)    
+    min_rsi = rsi.rolling(n).min()
+    max_rsi = rsi.rolling(n).max()
+    stoch_rsi = (rsi-min_rsi)/(max_rsi-min_rsi +1e-3)*100
+    return stoch_rsi
 
 
 class System(Strategy):
-    d_rsi = 30  # Daily RSI lookback periods
-    w_rsi = 30  # Weekly
-    level = 70
-    
+    d_rsi = 14  # Daily RSI lookback periods
+    level_up = 75
+    level_do = 10
     def init(self):
         # Compute moving averages the strategy demands
-        self.ma10 = self.I(SMA, self.data.Close, 10)
-        self.ma20 = self.I(SMA, self.data.Close, 20)
-        self.ma50 = self.I(SMA, self.data.Close, 50)
-        self.ma100 = self.I(SMA, self.data.Close, 100)
+        self.ma25 = self.I(SMA, self.data.Close, 10)
+        self.ma99 = self.I(SMA, self.data.Close, 50)
         
         # Compute daily RSI(30)
         self.daily_rsi = self.I(RSI, self.data.Close, self.d_rsi)
-        
-        # To construct weekly RSI, we can use `resample_apply()`
-        # helper function from the library
-        # self.weekly_rsi = resample_apply(
-        #     'W-FRI', RSI, self.data.Close, self.w_rsi)
+        # self.stoch_rsi = self.I(stoch_rsi, self.data.Close, self.d_rsi)
         
         
     def next(self):
         price = self.data.Close[-1]
-        
-        # If we don't already have a position, and
-        # if all conditions are satisfied, enter long.
-        # print(self.daily_rsi[-1] > self.level,
-        #     self.ma10[-1] > self.ma20[-1] > self.ma50[-1] > self.ma100[-1], price > self.ma10[-1])
+        curr_rsi = self.daily_rsi[-1]
 
-        if (not self.position and
-            self.daily_rsi[-1] > self.level and
-            self.ma10[-1] > self.ma20[-1] > self.ma50[-1] > self.ma100[-1] and price > self.ma10[-1]):
-            
-            # Buy at market price on next open, but do
-            # set 8% fixed stop loss.
-            self.buy(sl=.92 * price)
+        if repr(self._data).find('2022-06-16 07:00:00')>0:
+            print(self.position)
+            print(curr_rsi > self.level_up and price > self.ma25[-1] > self.ma99[-1])
+        # ipdb.set_trace()
+        # curr_sto_rsi = self.stoch_rsi[-1]
+        if not self.position:
+            if curr_rsi > self.level_up and price > self.ma25[-1] > self.ma99[-1] :
+                self.sell(sl = 1.04 *price, tp = 0.96 *price)
         
-        # If the price closes 2% or more below 10-day MA
-        # close the position, if any.
-        elif price < .98 * self.ma10[-1]:
-            self.position.close()
-
-# binance future api
-# api_key = "c21f1bb909318f36de0f915077deadac8322ad7df00c93606970441674c1b39b"
-# api_secret = "be2d7e74239b2e959b3446b880451cbb4dee2e6be9d391c4c55ae7ca0976f403"
-# testnet spot api
-# api_key = "6vHuJQ4RvEe2aEf2KJEdM3DuAOvfQ2qDV0AONlkdIG5f5f7poex3empbw8FqxK1W"
-# api_secret = "a86J1KCCeSebOneP23pWOviVXImkkRVaFwrJ7gi0eSCTEYOKQGBLNmRIOPlgldzB"
-# real api
+        
 api_key = "qCplIYtZsWN25xPIrljTBXB8pnRXIdgfiaAn0TypIsBNVyS5CdW1AFfTpjSnqsHv"
 api_secret = "3tau8BetzPiO9Tu1c2zfkTCeVD911qKHLZcixGarquR82lwZfIJ0BIwMyIPKpIsh"
 client = Client(api_key, api_secret, testnet=False)
@@ -81,12 +67,20 @@ print("USDT: ", balance)
 avg = []
 loss_day = {}
 win_day = {}
-month = "Apr"
-step = 3
+month = "Jul"
+year = 2022
+step = 25
+plot = False
+
+klines_m = client.get_historical_klines("BNBUSDT", '1M', f"1 {month}, {year}", f"30 {month}, {year}")
+O,H,L,C = klines_m[0][1:5]
+
 for i in range(1,30-step,step):
+# for i in range(16,17,step):
+    plot = True
     print("day ", i)
     klines = client.get_historical_klines(
-        "BNBUSDT", '5m', f"{i} {month}, 2021", f"{i + step} {month}, 2021")
+        "BNBUSDT", '1h', f"{i} {month}, {year}", f"{i + step} {month}, {year}")
     klines = [list(map(float, k)) for k in klines]
     df = pd.DataFrame(klines,
                       columns="Opentime, Open, High, Low, Close, Volume, Close time, Quote asset volume, Number of "
@@ -105,10 +99,12 @@ for i in range(1,30-step,step):
     elif stats.loc["Equity Final [$]"] < 1000:
         loss_day[str(i)] = stats.loc["Equity Final [$]"]
     avg.append(float(stats.loc["Equity Final [$]"]))
-    # print(stats.loc['_trades'])
-    # bt.plot()
+    if plot:
+        print(stats.loc['_trades'])
+        bt.plot()
     print("*******************\n")
 
+print(f"in {month} - {year} price change {100* (float(C)/float(O) - 1) :.3f}%")
 print("\n total pnl: ", sum(avg) - 1000*len(avg))
 print("win day: ", win_day)
 print("loss day: ", loss_day)
